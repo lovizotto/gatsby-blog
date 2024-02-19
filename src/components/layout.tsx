ed4970d03c9c57ad13c49a79f714640dd3cbf781
lovizotto <lovizotto@gmail.com>
2024-02-19 11:39:54 -0300

    Finishi the tutorial part 2

---
+++ src/components/layout.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import { container, navLinks, navLinkItem, navLinkText, heading} from './layout.module.css'
+
+interface LayoutProps {
+    pageTitle: string
+    children: React.ReactNode
+}
+const Layout = ({ pageTitle, children }: LayoutProps) => {
+    return (
+        <div className={container}>
+            <nav>
+                <ul className={navLinks}>
+                    <li className={navLinkItem}>
+                        <Link to="/" className={navLinkText}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className={navLinkItem}>
+                        <Link to="/about" className={navLinkText}>
+                            About
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+            <main>
+                <h1 className={heading}>{pageTitle}</h1>
+                {children}
+            </main>
+        </div>
+    )
+}
+
+export default Layout
