3f89c2faca9b527affbf7c948f78622befa00bda
lovizotto <lovizotto@gmail.com>
2024-02-19 16:44:01 -0300

    Finishi blog render - part 5

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,43 +1,51 @@
-import * as React from 'react'
-import { graphql } from 'gatsby'
-import Layout from '../components/layout'
-import Seo from '../components/seo'
+import * as React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
+import Seo from "../components/seo";
 
 interface BlogPageProps {
-    data: {
-        allFile: {
-            nodes: {
-                name: string
-            }[]
-        }
-    }
+  data: {
+    allMdx: {
+      nodes: {
+        frontmatter: {
+          date: string;
+          title: string;
+        };
+        id: string;
+        excerpt: string;
+      }[];
+    };
+  };
+}
 
-}
-const BlogPage = ({data}: BlogPageProps) => {
-    return (
-        <Layout pageTitle="My Blog Posts">
-            <ul>
-                {
-                    data.allFile.nodes.map(node => (
-                        <li key={node.name}>
-                            {node.name}
-                        </li>
-                    ))
-                }
-            </ul>
-        </Layout>
-    )
-}
+const BlogPage = ({ data }: BlogPageProps) => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <p>{node.excerpt}</p>
+        </article>
+      ))}
+    </Layout>
+  );
+};
 
 export const query = graphql`
-    query {
-        allFile {
-            nodes {
-                name
-            }
+  query {
+    allMdx(sort: { frontmatter: { date: DESC } }) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
         }
+        id
+        excerpt
+      }
     }
-`
-export const Head = () => <Seo title="My Blog Posts" />
+  }
+`;
+export const Head = () => <Seo title="My Blog Posts" />;
 
-export default BlogPage
+export default BlogPage;
